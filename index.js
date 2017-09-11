21224227b5537af3fb91e979ca9514e9ca33db2c
Peter Halliday <pghalliday@gmail.com>
2017-09-11 23:40:43 +0200

    set useragent and wait for javascript to run

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const puppeteer = require('puppeteer');
 
+const wait = (delay) => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve();
+  }, delay);
+});
+
 (async () => {
   const browser = await puppeteer.launch();
   try {
     const page = await browser.newPage();
-    const response = await page.goto('https://www.funda.nl/koop/1015sz/+1km/p1/');
-    const body = await response.text();
-    console.log(body);
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36');
+    await page.goto('https://www.funda.nl/koop/1015sz/+1km/p1/');
+    await wait(5000);
+    const text = await page.plainText();
+    console.log(text);
   } catch (e) {
     console.log(e);
   }
