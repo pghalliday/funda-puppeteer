9e52da6cbb382f3c40a3c522fad8d0e17fb45671
Peter Halliday <pghalliday@gmail.com>
2017-09-11 22:28:12 +0000

    get links

--- index.js
+++ index.js
@@ -11,10 +11,17 @@ const wait = (delay) => new Promise((resolve) => {
   try {
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36');
-    await page.goto('https://www.funda.nl/koop/1015sz/+1km/p1/');
+    await page.goto('https://www.funda.nl/koop/1015sz/+1km/');
     await wait(5000);
-    const text = await page.plainText();
-    console.log(text);
+    const pageLinks = await page.$$('div.pagination-pages > a');
+    const lastPageLink = pageLinks[pageLinks.length - 1];
+    const pageMax = await lastPageLink.evaluate(link => link.getAttribute('data-pagination-page'));
+    console.log(pageMax);
+    const resultLinks = await page.$$('div.search-result-media > a');
+    for (let resultLink of resultLinks) {
+      const href = await resultLink.evaluate(link => link.getAttribute('href'));
+      console.log(href);
+    }
   } catch (e) {
     console.log(e);
   }
